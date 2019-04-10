8c5f9a765aabc5a6622165767f8f268ccff87937
aaliyahthompson <49485686+aaliyahthompson@users.noreply.github.com>
2019-04-10 09:33:42 -0400

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,30 @@
+
+function whatToDo(dayOfWeek, weather) {
+    //Create a compound conditional statement below. 
+    
+    if(dayOfWeek === "Saturday" && weather2 === "sunny" ){
+    $("#result").html("I'll go for a bike ride.");
+    
+    }else if(  ){
+    
+    
+        }else if(  ){
+    
+            }else if(  ){
+
+                        }else()
+});
+
+    //These three lines will have to be used in the conditional statement. Feel free to re-arrange.
+    $("#result").html("I'll go for a bike ride.");
+    $("#result").html("I'll go binge watch Netflix.");
+    $("#result").html("I'll go to the park.");
+
+} 
+
+$("button").click(function(){
+    var dayOfWeek = $("#dayOfWeek").val();
+    var weather = $("#weather").val();
+    whatToDo(dayOfWeek, weather);   
+var day = dayOfWeek;
+var weather2 = weather;
